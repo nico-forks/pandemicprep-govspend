8ccf6100f98b2374b491bd1ce44d255b1024abd6
Nicolas Olivares <nolivaresr@hotmail.com>
2021-03-26 14:10:11 -0400

    undid the recent changes

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ server.use((req, res, next) => {
 const { client } = require('./db/client');
 
 // connect to the server
-// const PORT = process.env.PORT || 5000;
-const PORT = process.env.DATABASE_URL || 5000;
+const PORT = process.env.PORT || 5000;
+// const PORT = process.env.DATABASE_URL || 5000;
 server.listen(PORT, async () => {
   console.log(`Server is running on ${ PORT }!`);
 
