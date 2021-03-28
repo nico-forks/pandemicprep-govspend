3ff13a307db43ed88726b05b54ad6f9c46d099dc
Nicolas Olivares <nolivaresr@hotmail.com>
2021-03-27 22:18:19 -0400

    fixed ssl issue for heroku

--- db/client.js
+++ db/client.js
@@ -1,7 +1,15 @@
 const { Client } = require('pg');
 const DB_NAME = 'localhost:5432/doomsday'
 const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
-const client = new Client(DB_URL);
+
+//const client = new Client(DB_URL); needed during development
+
+const client = new Client({
+  connectionString: DB_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = {
   client,
